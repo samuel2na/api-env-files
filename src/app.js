3947163c62ef759657ae099ca8f550c4501c306c
samuel2na <samuel.sb05g@gmail.com>
2025-06-22 20:20:29 -0300

    ajuste CORS

--- src/app.js
+++ src/app.js
@@ -6,14 +6,22 @@ import morgan from "morgan";
 // Importa a instância do Multer e a função controladora
 import { uploadMiddleware, uploadController } from "./upload.js"; // <-- ALTERADO AQUI
 
+const allowedOrigins = process.env.CORS_ORIGINS.split(',');
+
 const app = express();
 
 app.use(helmet()); // Security middleware to set various HTTP headers
 app.use(express.json()); // Middleware to parse JSON bodies
 app.use(morgan("dev")); // HTTP request logger middleware
-// app.use(cors(
-//     { origin: process.env.CORS_ORIGIN, }
-// ));
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
 
 // Define a rota de upload, encadeando o middleware do Multer com o controlador
 app.post("/upload", uploadMiddleware, uploadController); // <-- ALTERADO AQUI
