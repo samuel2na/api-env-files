47246dd378d40c49566fcd010c0d1e0fc5c3a20f
samuel2na <samuel.sb05g@gmail.com>
2025-06-22 22:20:47 -0300

    ajuste comentando const handler

--- api/index.js
+++ api/index.js
@@ -3,4 +3,5 @@ import serverless from 'serverless-http';
 
 import app from '../src/app.js'; // reaproveita a configuração do Express
 
-export const handler = serverless(app);
+//export const handler = serverless(app);
+export default serverless(app); // ✅ obrigatório para Vercel
