b500872eed579b76b8cdadb2451a2ae69f11f90b
samuel2na <samuel.sb05g@gmail.com>
2025-06-23 09:50:24 -0300

    remove pasta api

--- api/index.js
+++
@@ -1,7 +0,0 @@
-// api/index.js
-import serverless from 'serverless-http';
-
-import app from '../src/app.js'; // reaproveita a configuração do Express
-
-//export const handler = serverless(app);
-export default serverless(app); // ✅ obrigatório para Vercel
